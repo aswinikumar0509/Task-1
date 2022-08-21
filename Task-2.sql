b233c2c28e3969a13e7ff261c4e803d7f760936b
Aswini Kumar <64352951+aswinikumar0509@users.noreply.github.com>
2022-08-21 11:04:22 +0530

    Add files via upload

---
+++ Task-2.sql
@@ -0,0 +1,15 @@
+create database task2;
+
+use task2;
+
+show tables;
+
+select * from fitbit;
+
+select date_format(ActivityDate,"%Y")from fitbit;
+select date_format(ActivityDate,"%m")from fitbit;
+select date_format(ActivityDate,"%d")from fitbit;
+
+select  distinct(id)  from fitbit;
+
+
